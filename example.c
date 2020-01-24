0346e314f98fa8a834ec46176ceaf90fdb3d519f
Evan Teran <evan.teran@gmail.com>
2020-01-24 11:00:33 -0500

    fixing build of example too!

--- example.c
+++ example.c
@@ -10,50 +10,52 @@
 
 int main(int argc, char *argv[]) {
 
-    /* this is the variable that will store the array, you can have
+	/* this is the variable that will store the array, you can have
      * a vector of any type! For example, you may write float *v = NULL,
      * and you'd have a vector of floats :-). NULL will have a size
      * and capacity of 0. additionally, vector_begin and vector_end will
-     * return NULL on a NULL vector.
+     * return NULL on a NULL vector. Alternatively, for clarity of writing
+     * use can use the cvector_vector_type macro to define a vector of a
+     * given type.
      */
-    cvector_vector_type(int) v = NULL;
+	cvector_vector_type(int) v = NULL;
 
-    (void)argc;
-    (void)argv;
+	(void)argc;
+	(void)argv;
 
-    /* add some elements to the back */
-    cvector_push_back(v, 10);
-    cvector_push_back(v, 20);
-    cvector_push_back(v, 30);
+	/* add some elements to the back */
+	cvector_push_back(v, 10);
+	cvector_push_back(v, 20);
+	cvector_push_back(v, 30);
 
-    /* and remove one too */
-    cvector_pop_back(v);
+	/* and remove one too */
+	cvector_pop_back(v);
 
-    /* print out some stats about the vector */
-    printf("pointer : %p\n", (void *)v);
-    printf("capacity: %lu\n", cvector_capacity(v));
-    printf("size    : %lu\n", cvector_size(v));
+	/* print out some stats about the vector */
+	printf("pointer : %p\n", (void *)v);
+	printf("capacity: %lu\n", cvector_capacity(v));
+	printf("size    : %lu\n", cvector_size(v));
 
-    /* iterator over the vector using "iterator" style */
-    if (v) {
-        int *it;
-        int i = 0;
-        for (it = cvector_begin(v); it != cvector_end(v); ++it) {
-            printf("v[%d] = %d\n", i, *it);
-            ++i;
-        }
-    }
+	/* iterator over the vector using "iterator" style */
+	if (v) {
+		int *it;
+		int i = 0;
+		for (it = cvector_begin(v); it != cvector_end(v); ++it) {
+			printf("v[%d] = %d\n", i, *it);
+			++i;
+		}
+	}
 
-    /* iterator over the vector standard indexing too! */
-    if (v) {
-        size_t i;
-        for (i = 0; i < cvector_size(v); ++i) {
-            printf("v[%lu] = %d\n", i, v[i]);
-        }
-    }
+	/* iterator over the vector standard indexing too! */
+	if (v) {
+		size_t i;
+		for (i = 0; i < cvector_size(v); ++i) {
+			printf("v[%lu] = %d\n", i, v[i]);
+		}
+	}
 
-    /* well, we don't have destructors, so let's clean things up */
-    cvector_free(v);
+	/* well, we don't have destructors, so let's clean things up */
+	cvector_free(v);
 
-    return 0;
+	return 0;
 }
